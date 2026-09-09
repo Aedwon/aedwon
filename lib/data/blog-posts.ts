02e76b99d899d45153cab47cfef87bba4a060a5d
Aerol <52475561+Aedwon@users.noreply.github.com>
2026-09-09 18:00:29 +0800

    Aggregate published blog posts [skip ci]

---
+++ lib/data/blog-posts.ts
@@ -0,0 +1,4 @@
+import { NEXT_MODEL_BLOG } from './blog-next-model';
+import { BLOG_POSTS as EXISTING_BLOG_POSTS } from './blogs';
+
+export const BLOG_POSTS = [NEXT_MODEL_BLOG, ...EXISTING_BLOG_POSTS];
